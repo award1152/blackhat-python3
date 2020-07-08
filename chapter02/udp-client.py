4b193b78e7259863efd43388407c1b1c5e8a3e8f
award1152 <65471849+award1152@users.noreply.github.com>
2020-07-08 10:30:59 -0500

    Update udp-client.py
    When running the original code I found out there was something wrong because nothing was printing, not sure if everyone else gets this error but the fix was to bind the target address to the socket.

--- chapter02/udp-client.py
+++ chapter02/udp-client.py
@@ -6,6 +6,9 @@ target_port = 80
 # create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+#typo: missing binding socket to target address (must run as root)
+client.bind((target_host,target_port))
+
 # send some data
 client.sendto(b"AAABBBCCC", (target_host, target_port))
 
